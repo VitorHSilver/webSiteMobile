a8484503fd7709a56e7ee9eff2bc2b44f09648e8
VitorHSilver <vitor-henrique1996@hotmail.com>
2025-01-02 18:37:42 -0300

    Adiciona pop-up informativo para reduzir a página em mobile

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,3 +1,23 @@
+setTimeout(() => {
+     const popup = document.createElement('div');
+     popup.style.borderRadius = '4px'
+     popup.style.position = 'fixed';
+     popup.style.top = '50%';
+     popup.style.left = '50%';
+     popup.style.transform = 'translate(-50%, -50%)';
+     popup.style.padding = '20px';
+     popup.style.backgroundColor = 'white';
+     popup.style.border = '1px solid black';
+     popup.style.zIndex = '1000';
+     popup.innerText = 'Por favor, diminua a página';
+
+     document.body.appendChild(popup);
+
+     // Remove o pop-up após 5 segundos
+     setTimeout(() => {
+          document.body.removeChild(popup);
+     }, 5000);
+}, 8000);
 class MobileNavBar {
      constructor(mobileMenu, navList, navLinks) {
           this.mobileMenu = document.querySelector(mobileMenu);
